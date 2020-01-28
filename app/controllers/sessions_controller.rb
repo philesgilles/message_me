a7265fadb37f28182ca7af63c2251059a8cf2816
Philippe Gilles <philgdep@gmail.com>
2020-01-28 09:57:48 +0100

    add create message controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
+
   def new
   end
 
@@ -24,6 +26,10 @@ class SessionsController < ApplicationController
 
   private
 
-  def user_params
+  def logged_in_redirect
+    if logged_in?
+      flash[:warning] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 end
